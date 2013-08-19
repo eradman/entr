361eb8239f87a6ca2f9e32b15a172040ad23a25a
Eric Radman <ericshane@eradman.com>
2013-08-19 01:57:54 -0400

    Define architecture in Makefile instead of testinf for __linux or __APPLE__. use strlcpy.c from OpenBSD

--- missing/compat.h
+++ missing/compat.h
@@ -1,16 +1,14 @@
 /* compat.h */
 
-#if defined(__linux)
+#if defined(_LINUX_PORT)
 #include <sys/types.h>
 
-#undef strlcpy
 size_t strlcpy(char *to, const char *from, int l);
 #endif
 
 
-#if defined(__APPLE__)
+#if defined(_MACOS_PORT)
 #include <stdio.h>
 
-#undef fmemopen
 FILE *fmemopen(void *buf, size_t size, const char *mode);
 #endif
