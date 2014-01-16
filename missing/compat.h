37eef2b5774c5beee99a24fd2c2a7279f68a6503
Eric Radman <ericshane@eradman.com>
2014-01-16 15:09:15 -0500

    Raise ARG_MAX from 4K to 2MB on Linux. sysconfig(_SC_ARG_MAX) does not seem to work.

--- missing/compat.h
+++ missing/compat.h
@@ -2,20 +2,15 @@
 
 #if defined(_LINUX_PORT)
 #include <sys/types.h>
-
 size_t strlcpy(char *to, const char *from, int l);
-void setproctitle(const char *fmt, ...);
-void compat_init_setproctitle(int argc, char *argv[]);
 #endif
 
 
 #if defined(_MACOS_PORT)
 #include <stdio.h>
-
-void setproctitle(const char *fmt, ...);
 FILE *fmemopen(void *buf, size_t size, const char *mode);
 #endif
 
 #if !defined(ARG_MAX)
-#define ARG_MAX 4096
+#define ARG_MAX 2097152
 #endif
