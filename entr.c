30cc34d314b2ed7606c669c2393fc37578582dff
Leonardo Taccari <iamleot@gmail.com>
2020-06-15 19:54:31 +0200

    Always wait childs to avoid zombies (#38)

--- entr.c
+++ entr.c
@@ -254,8 +254,8 @@ void
 proc_exit(int sig) {
 	int status;
 
+	xwaitpid(child_pid, &status, 0);
 	if ((oneshot_opt == 1) && (terminating == 0)) {
-		xwaitpid(child_pid, &status, 0);
 		if ((shell_opt == 1) && (restart_opt == 0)) {
 			fprintf(stdout, "%s returned exit code %d\n",
 			    basename(getenv("SHELL")), WEXITSTATUS(status));
