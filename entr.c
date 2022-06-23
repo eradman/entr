eradman/entr

commit 7c89dfb2b442550c7892d9358d876da36f53d173

Use a closed pipe for stdin with -r

entr.c
@@ -400,9 +400,15 @@ run_utility(char *argv[]) {
 	char **new_argv;
 	char *p, *arg_buf;
 	int argc;
+	int stdin_pipe[2];
 
-	if (restart_opt == 1)
+	if (restart_opt == 1) {
 		terminate_utility();
+
+		if (pipe(stdin_pipe) != 0)
+			err(1, "Failed to create stdin pipe");
+		close(stdin_pipe[1]);
+	}
 
 	if (shell_opt == 1) {
 		/* run argv[1] with a shell using the leading edge as $0 */
@@ -444,7 +450,7 @@ run_utility(char *argv[]) {
 		/* Set process group so subprocess can be signaled */
 		if (restart_opt == 1) {
 			setpgid(0, getpid());
-			close(STDIN_FILENO);
+			dup2(stdin_pipe[0], STDIN_FILENO);
 		}
 		/* wait up to 1 seconds for each file to become available */
 		for (i=0; i < 10; i++) {

commit b22652ed3ef13242089911dddad4c2ff181deee9

Close also read end of the pipe when it is no longer needed
entr creates pipe in an -r mode to a child process, however, the read
end of the pipe needs to be closed in parent process after fork,
otherwise file descriptors will be exhausted after some time and enter
will die with an error:

   entr: Failed to create stdin pipe: No file descriptors available

Additionally, it is also common to close read end of the pipe in child
process after it was duplicated to standard input file descriptor.

The issue is easily reproducible by:

  1. limiting number of file descriptors to lower number,
  2. running entr in -r mode and
  3. hitting the space button.

In Linux:

  $ ulimit -n 16
  $ echo entr | ./entr -r date

Fixes: 7c89dfb2b442 ("Use a closed pipe for stdin with -r")

entr.c
@@ -496,6 +496,7 @@ run_utility(char *argv[]) {
 		if (restart_opt == 1) {
 			setpgid(0, getpid());
 			dup2(stdin_pipe[0], STDIN_FILENO);
+			close(stdin_pipe[0]);
 		}
 		/* wait up to 1 seconds for each file to become available */
 		for (i=0; i < 10; i++) {
@@ -508,6 +509,9 @@ run_utility(char *argv[]) {
 			err(1, "exec %s", new_argv[0]);
 	}
 	child_pid = pid;
+
+	if (restart_opt == 1)
+		close(stdin_pipe[0]);
 
 	if (restart_opt == 0 && oneshot_opt == 0) {
 		if (wait(&status) != -1)