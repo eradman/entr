893f865d165e28145157ac11cf807cd141c38ae1
wonjae-ai <lwoj3019515@gmail.com>
2025-11-14 15:05:38 +0900

    Revert "feat(inotify): Implement inotify backend for Linux and remove kqueue support"
    This reverts commit 18dae68f7dcb46d013942ef8c29e2e9afdefc733.

--- project/event.h
+++ project/event.h
@@ -5,15 +5,16 @@
 
 #include "entr.h"
 
-WatchFile *wd_to_file(int wd);
+// Kqueue 백엔드 (BSD/macOS)
 int event_init(void);
-void watch_file(int event_fd, WatchFile *file);
+int event_watch(int event_fd, WatchFile *file);
 void event_loop(int event_fd, char *argv[]);
+void event_exit(int event_fd);
 
 // Inotify 백엔드 (Linux)
 extern int inotify_init(void);
 extern int inotify_watch(int event_fd, WatchFile *file);
 extern void inotify_loop(int event_fd, char *argv[]);
 extern void inotify_exit(int event_fd);
-int event_init(void);
+
 #endif // PROJECT_EVENT_H
